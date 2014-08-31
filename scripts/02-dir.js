12db42ebf3d4e8b096133bb12e229e7543cc3267
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-08-31 17:20:47 +0200

    add console.dir

---
+++ scripts/02-dir.js
@@ -0,0 +1,11 @@
+console.info('console.log object');
+console.log({ name: 'Niklas' });
+
+console.info('console.dir object');
+console.dir({ name: 'Niklas' });
+
+console.info('console.log document.body');
+console.log(document.body);
+
+console.info('console.dir document.body');
+console.dir(document.body);
