82ab28b364e258098b889a6be54404412a8caf77
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-08-31 17:39:49 +0200

    add console.table

---
+++ scripts/05-table.js
@@ -0,0 +1,20 @@
+console.table([{a:1, b:2, c:3}, {a:"foo", b:false, c:undefined}]);
+
+// console.table([[1,2,3], [2,3,4]]);
+
+// // function Person(firstName, lastName, age) {
+// //   this.firstName = firstName;
+// //   this.lastName = lastName;
+// //   this.age = age;
+// // }
+
+// // var family = {};
+
+// // family.mother = new Person("Susan", "Doyle", 32);
+// // family.father = new Person("John", "Doyle", 33);
+// // family.daughter = new Person("Lily", "Doyle", 5);
+// // family.son = new Person("Mike", "Doyle", 8);
+
+// // console.table(family);
+
+// // // console.table(family, ["firstName", "lastName"]);
