14d6439a906a3071ad038a7e319496b4fe5ecc74
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-08-31 16:36:55 +0200

    add console.time

---
+++ scripts/02-time.js
@@ -0,0 +1,12 @@
+function initArray() {
+  console.debug("Initializing array...");
+  console.time("Array initialized in");
+
+  var array = new Array(1000000);
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    array[i] = new Object();
+  };
+
+  console.timeEnd("Array initialized in");
+}
