4e8ae98212af3d69cfca2f83c44942677fd02b36
Niklas Rasmusson <niklas.rasmusson@gmail.com>
2014-08-31 16:26:09 +0200

    add console.debug

--- scripts/01-logs.js
+++ scripts/01-logs.js
@@ -7,10 +7,15 @@ var myObj = {
 var l33t = 1337;
 
 function error() {
-  console.error('Woops!');
+  function makeError() {
+    console.error('Woops!');
+  }
+  makeError();
 }
 
 console.log("%s %O", "I'm logging an object", myObj);
+
+console.debug("Debugging...");
 
 console.info("This is so %i", l33t);
 
